7cd0ec913cf472b6d1f0a1630fb77c4e0939de03
James Fish <james@fishcakez.com>
2014-07-19 12:00:55 +0100

    Relax elixir requirement to 0.14.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Dialyze.Mixfile do
   def project do
     [app: :dialyze,
      version: "0.0.1",
-     elixir: "~> 0.14.2",
+     elixir: "~> 0.14.0",
      deps: deps,
      aliases: [install: ["compile", "archive.install --force"]]]
   end

cef32dcfd9db0505f6d04d9e3930a72f1155838c
James Fish <james@fishcakez.com>
2014-07-19 11:32:40 +0100

    Add mix alias install
    Can install with `mix install`

--- mix.exs
+++ mix.exs
@@ -5,7 +5,8 @@ defmodule Dialyze.Mixfile do
     [app: :dialyze,
      version: "0.0.1",
      elixir: "~> 0.14.2",
-     deps: deps]
+     deps: deps,
+     aliases: [install: ["compile", "archive.install --force"]]]
   end
 
   def application do

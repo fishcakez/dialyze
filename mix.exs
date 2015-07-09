6f2db59e29cd341539d13df3db1fa3fb687cc7ea
James Fish <james@fishcakez.com>
2015-07-09 15:05:42 +0100

    Fix mix.exs

--- mix.exs
+++ mix.exs
@@ -3,8 +3,8 @@ defmodule Dialyze.Mixfile do
 
   def project do
     [app: :dialyze,
-     version: "0.1.4-min_plt",
-     elixir: "~> 0.14.3 or ~> 0.15.0 or ~> 1.0.0 or ~> 1.1.0-dev",
+     version: "0.2.0",
+     elixir: "~> 0.14.3 or ~> 0.15.0 or ~> 1.0",
      description: "Dialyzer mix task",
      deps: [],
      aliases: [install: ["compile", "archive.build", "archive.install --force"]],
